eacc2f41f9815983ed380de493de229847147413
Emin Tahirov <emintahirov1996@gmail.com>
2022-11-01 18:41:46 -0600

    added get functionality

--- server/controllers/hairControllers.js
+++ server/controllers/hairControllers.js
@@ -5,22 +5,26 @@ const hairController = {};
 hairController.getInfo = (req, res, next) => {
     // write code here
     //use db.query
-    const queryString = 'SELECT a.*, b.name as species, c.name as homeworld FROM people a FULL OUTER JOIN species b ON a.species_id = b._id FULL OUTER JOIN planets c ON a.homeworld_id = c._id'
-  
+    // ` ${ }   `
+    console.log('hit')
+    let username = 'eminthrv'
+    //let username = req.params.id;
+    const queryString = `SELECT * FROM hair WHERE username = ${username}`
+    console.log(username)
     return db
       .query(queryString)
       .then(resp => {
         console.log(resp)
-        res.locals.getCharacters = resp.rows
+        res.locals.getInfo = resp
         // db.release()
         return next()
       })
       .catch(e => {
         next({
-          log: 'error in middleware getCharacters',
-          msg: { err: 'check middleware getCharacters' }
+          log: 'error in middleware getInfo',
+          msg: { err: 'check middleware getInfo' }
         })
       })
   };
 
-  
+module.exports =  hairController
